8c790a3f46f7ed4b22b7f7f6a58e2625ea5b7b61
RMrobb1e <robbie@roofbuddy.co.nz>
2024-03-05 07:54:46 +0800

    chore: remove comment

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  // Routes that can be accessed while signed out
   publicRoutes: ["/anyone-can-visit-this-route"],
   // Routes that can always be accessed, and have
   // no authentication information
