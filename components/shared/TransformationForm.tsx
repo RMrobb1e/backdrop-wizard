ecf2f84a09e4938652c65b87beb6fca445c47514
RMrobb1e <robbiemariano126@gmail.com>
2024-03-19 21:49:51 +0800

    feat: fix debounce not being invoke

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -190,7 +190,7 @@ const TransformationForm = ({
           [fieldName === "prompt" ? "prompt" : "to"]: value,
         },
       }));
-    }, 1000);
+    }, 1000)();
 
     return onChangeField(value);
   };
--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        domain: "res.cloudinary.com",
+        hostname: "res.cloudinary.com",
         port: "",
       },
     ],
