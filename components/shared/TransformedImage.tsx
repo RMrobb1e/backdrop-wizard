facecf44b9a6163a760d10cbce675baf6bbe6c1f
RMrobb1e <robbiemariano126@gmail.com>
2024-03-19 21:46:02 +0800

    feat: implement update page

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -63,7 +63,7 @@ const TransformedImage = ({
             onError={() => {
               debounce(() => {
                 setIsTransforming && setIsTransforming(false);
-              }, 800);
+              }, 8000)();
             }}
             {...transformationConfig}
           />
@@ -73,8 +73,9 @@ const TransformedImage = ({
                 src="/assets/icons/spinner.svg"
                 width={50}
                 height={50}
-                alt="Transforming"
+                alt="spinner"
               />
+              <p className="text-white/80">Please wait...</p>
             </div>
           )}
         </div>
